cfc761caec26428d7aece69da93c26fb05e48c4b
TiagoPiovesan <tiago.piovesan.tp@gmail.com>
2019-05-26 13:16:44 -0300

    add STATIC_ROOT

--- core/settings.py
+++ core/settings.py
@@ -101,8 +101,7 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-
-# STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'login'
